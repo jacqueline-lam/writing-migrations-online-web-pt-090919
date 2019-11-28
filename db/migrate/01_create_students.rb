48b0ab4ea2ffc599bc094f1b342b1c6b7b865ceb
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-11-28 06:40:03 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     # create tables using Ruby only
     create_table :students do |t|
-      t.string :title
+      t.string :name
     end
   end
 end
